6741fac6fad0b89cf498988b4827b4f832e5daa7
Dewey Hong <doohee323@gmail.com>
2014-02-04 18:27:20 -0800

    .

---
+++ src/main/java/com/tz/quiz/service/DrinkingService.java
@@ -0,0 +1,127 @@
+package com.tz.quiz.service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.tz.quiz.domain.Player;
+import com.tz.quiz.domain.Status;
+import com.tz.quiz.support.Constants;
+
+/**
+ * <pre>
+ * business class for the app.
+ * </pre>
+ * 
+ */
+public class DrinkingService {
+
+	private Status status = new Status();  // status for app.
+	private int pausetime = Constants.defaultRollSpeed; // rolling time
+	private int maxDrinkingCnt = Constants.defaultMaxDrinkingCnt; // maximum
+
+	/**
+	 * <pre>
+	 * clone players except for self in order to manipulate player list
+	 * </pre>
+	 * 
+	 * @param List
+	 *            <Player> players participants of game
+	 * @return Status status for app.
+	 */
+	public Status playDrinkingGame(List<Player> players) {
+
+		try {
+			// play with random roll or not
+			if (Constants.radomPlay) {
+				Collections.shuffle(players);
+			}
+			status.setPlayers(players);
+			status.setMaxDrinkingCnt(maxDrinkingCnt);
+			status.setPausetime(pausetime);
+
+			int nSecond = 0; // time by second
+			while (true) {
+
+				// if only one player is left, game finish
+				if (status.getPlayers().size() < 2) {
+					break;
+				}
+				// status.setbWin(false);
+				status.setnSecond(nSecond);
+
+				Thread.sleep(100);
+
+				ExecutorService pool = Executors.newFixedThreadPool(status
+						.getPlayers().size());
+				Set<Future<Status>> set = new HashSet<Future<Status>>();
+
+				int nTurn = status.getnTurn();
+				for (int i = 0; i < status.getPlayers().size(); i++) {
+					Player player = status.getPlayers().get(i);
+					if (nTurn == player.getSn()) {
+						player.setbTurn(true);
+					} else {
+						player.setbTurn(false);
+					}
+					player.setStatus(status);
+					Callable<Status> callable = player;
+					Future<Status> future = pool.submit(callable);
+					set.add(future);
+				}
+
+				boolean bWin = false;
+				for (Future<Status> future : set) {
+					try {
+						status = future.get();
+						if (status.isbWin()) {
+							bWin = true;
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (ExecutionException e) {
+						e.printStackTrace();
+					}
+				}
+				status.setbWin(bWin);
+
+				pool.shutdown();
+
+				nSecond++;
+			}
+
+			// print status
+			status.logEnd();
+
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public int getPausetime() {
+		return pausetime;
+	}
+
+	public void setPausetime(int pausetime) {
+		this.pausetime = pausetime;
+	}
+
+	public int getMaxDrinkingCnt() {
+		return maxDrinkingCnt;
+	}
+
+	public void setMaxDrinkingCnt(int maxDrinkingCnt) {
+		this.maxDrinkingCnt = maxDrinkingCnt;
+	}
+
+}
---
+++ src/main/java/com/tz/quiz/support/Constants.java
@@ -0,0 +1,102 @@
+package com.tz.quiz.support;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import com.tz.quiz.domain.Player;
+import com.tz.quiz.domain.Status;
+
+/**
+ * <pre>
+ * constant variables and static method for utility
+ * </pre>
+ * 
+ */
+public class Constants {
+
+	// winning cases
+	final public static String[] winningNumberSet = { "1,6", "2,5", "3,4",
+			"4,3", "5,2", "6,1", "5,6", "6,5", "1,1", "2,2", "3,3", "4,4",
+			"5,5", "6,6" };
+	// default rolling speed
+	final public static int defaultRollSpeed = 2;
+	// default maximum count which player can drink in this rolling
+	final public static int defaultMaxDrinkingCnt = 2;
+	// for development, whether using random player or not
+	public static boolean radomPlay = true;
+	// debugger flag
+	public static boolean debug = true;
+
+	/**
+	 * <pre>
+	 * whether the input value is winning value or not
+	 * </pre>
+	 * 
+	 * @param String
+	 *            input return value of rolling game
+	 * @return is winning value or not
+	 */
+	public static boolean isWin(String input) {
+		List<String> winSet = new ArrayList<String>();
+		winSet.addAll(Arrays.asList(winningNumberSet));
+		return winSet.contains(input);
+	}
+
+	/**
+	 * <pre>
+	 * clone players except for self in order to manipulate player list
+	 * </pre>
+	 * 
+	 * @param List
+	 *            <Player> input player to be cloned
+	 * @param String
+	 *            except except for self
+	 * @return List<Player> cloned player
+	 */
+	public static List<Player> clonePlayers(List<Player> input, String except) {
+		List<Player> players = new ArrayList<Player>();
+		Iterator<Player> e = input.iterator();
+		while (e.hasNext()) {
+			Player player = e.next();
+			if (!player.getPlayerName().equals(except)) {
+				players.add(player);
+			}
+		}
+		return players;
+	}
+
+	/**
+	 * <pre>
+	 * find the next player who is'nt drinking
+	 * </pre>
+	 * 
+	 * @param Roll
+	 *            roll <Player> current roll
+	 * @return Roll roll <Player> current roll
+	 */
+	public static Status findNextDicer(Status status) {
+		int nTurn = status.getnTurn();
+		// if there is no one drinking, turn change.
+		if (status.getLeftDrintCnt() == 0) {
+			nTurn++;
+		}
+
+		// if the next player is drinking, then the turn pass to the next one.
+		// nTurn++;
+		// for (int i = nTurn; i < roll.getPlayers().size(); i++) {
+		// if(roll.getPlayers().get(i).getLeftDrinkingTime() == 0) {
+		// break;
+		// } else {
+		// nTurn++;
+		// }
+		// }
+
+		if (nTurn >= status.getPlayers().size()) {
+			nTurn = 0;
+		}
+		status.setnTurn(nTurn);
+		return status;
+	}
+}
